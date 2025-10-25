d8bf4bd32c79f1fca2be9ac4f07b361a6dead562
ye5m4r <yesmarortizz@gmail.com>
2025-10-25 17:47:19 -0500

    revertí un cambio al main que no debí hacer

--- EcommerceUIS/client/src/main.jsx
+++ EcommerceUIS/client/src/main.jsx
@@ -5,9 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './components/App.jsx'
 import { CartProvider } from './contexts/CartContext.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 createRoot(document.getElementById('root')).render(
